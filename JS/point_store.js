f5273f5be616478d98384b8a174b9b0593c58a4c
Eugene16342 <WOW23875708@gmail.com>
2024-06-18 21:52:11 +0800

    更新了點數商城商品卡片的生成函式

--- JS/point_store.js
+++ JS/point_store.js
@@ -148,28 +148,23 @@ document.addEventListener("DOMContentLoaded", function () {
     containerRow.innerHTML = htmlStr;
   }
 
-  function setupScrolling(
-    containerSelector,
-    nextBtnSelector,
-    preBtnSelector,
-    scrollAmount
-  ) {
-    let container = document.querySelector(containerSelector);
-    let nextBtn = document.querySelector(nextBtnSelector);
-    let preBtn = document.querySelector(preBtnSelector);
+  function setupScrolling(scrollSpeed) {
+    let container = document.querySelector(".Store_Container");
+    let nextBtn = document.querySelector(".btn_next");
+    let preBtn = document.querySelector(".btn_pre");
 
     let scrollInterval; // 用於存儲setInterval的ID
 
     // 為按鈕添加mousedown事件監聽器
     nextBtn.addEventListener("mousedown", function () {
       scrollInterval = setInterval(function () {
-        container.scrollLeft += scrollAmount;
+        container.scrollLeft += scrollSpeed;
       }, 15); // 每20毫秒滑動一次
     });
 
     preBtn.addEventListener("mousedown", function () {
       scrollInterval = setInterval(function () {
-        container.scrollLeft -= scrollAmount;
+        container.scrollLeft -= scrollSpeed;
       }, 15);
     });
 
@@ -189,35 +184,103 @@ document.addEventListener("DOMContentLoaded", function () {
     preBtn.addEventListener("mouseleave", function () {
       clearInterval(scrollInterval);
     });
-
-    //自動滾動功能看起來有點卡卡的且看不清卡片的內容有點雞肋，
-    //新想法讓他快速向右一大段距離，類似輪播圖效果，但是一次切換多張卡片
-    // function autoScroll() {
-    //   if (
-    //     container.scrollLeft >=
-    //     container.scrollWidth - container.clientWidth
-    //   ) {
-    //     scrollAmount = -Math.abs(scrollAmount); // 到達右邊界，改為向左滾動
-    //   } else if (container.scrollLeft <= 0) {
-    //     scrollAmount = Math.abs(scrollAmount); // 到達左邊界，改為向右滾動
-    //   }
-    //   container.scrollLeft += scrollAmount;
-    // }
-
-    // // 啟動自動滾動
-    // autoScrollInterval = setInterval(autoScroll, 30);
-
-    // // 當滑鼠進入容器時，停止自動滾動
-    // container.addEventListener("mouseenter", function () {
-    //   clearInterval(autoScrollInterval);
-    // });
-
-    // // 當滑鼠離開容器時，重新啟動自動滾動
-    // container.addEventListener("mouseleave", function () {
-    //   autoScrollInterval = setInterval(autoScroll, 20);
-    // });
   }
 
+  let goods = [
+    {
+      img: "./img/HomePage/goods1.jpg",
+      name: "VR頭盔之類的",
+      price: 12300,
+    },
+    {
+      img: "./img/HomePage/goods2.jpg",
+      name: "遊戲手把之類的",
+      price: 5678,
+    },
+    {
+      img: "./img/HomePage/goods3.jpg",
+      name: "很大的滑鼠墊",
+      price: 1234,
+    },
+    {
+      img: "./img/HomePage/goods4.jpg",
+      name: "應該是音響吧我也不確定",
+      price: 9876,
+    },
+    {
+      img: "./img/HomePage/goods5.jpg",
+      name: "叉叉盒子通行證",
+      price: 599,
+    },
+    {
+      img: "./img/HomePage/goods6.jpg",
+      name: "某點卡",
+      price: 450,
+    },
+    {
+      img: "./img/HomePage/goods7.jpg",
+      name: "賴點卡",
+      price: 60,
+    },
+    {
+      img: "./img/HomePage/goods8.jpg",
+      name: "日本大廠點卡",
+      price: 1000,
+    },
+    {
+      img: "./img/HomePage/goods9.jpg",
+      name: "美國大廠點卡",
+      price: 3000,
+    },
+    {
+      img: "./img/HomePage/goods10.jpg",
+      name: "看起來像中空的滑鼠",
+      price: 9999,
+    },
+    {
+      img: "./img/HomePage/goods11.jpg",
+      name: "看起來很站位置的鍵盤",
+      price: 8888,
+    },
+    {
+      img: "./img/HomePage/goods12.jpg",
+      name: "耳機",
+      price: 11111,
+    },
+  ];
+
+  function setGoodsCards(title, goodsSort, cardsContainer) {
+    let container = document.querySelector(cardsContainer);
+    let cardsHTML = "";
+    // 使用 forEach 方法來遍歷 goodsSort 陣列
+    goodsSort.forEach((item) => {
+      cardsHTML += `
+            <div class="card col-3">
+                <img src="${item.img}" class="filter card-img-top">
+                <div class="card-body">
+                    <a href="" class="card-text">${item.name}</a>
+                    <span class="Goods_Info_Price">${item.price}</span>
+                </div>
+                <a class="put_in_order">放入購物車</a>
+            </div>
+        `;
+    });
+
+    container.innerHTML = `
+    <div class="goods_cards_nav">
+        <h4 class="sorts_title">${title}</h4>
+        <a class="watch_more" href="">看更多</a>
+    </div>
+    <div class="goods_cards">
+        ${cardsHTML}  <!-- 將生成的卡片的 HTML 字串插入到這裡 -->
+    </div>
+`;
+  }
+
+  setGoodsCards("熱賣商品", goods, ".goodsSort1");
+  setGoodsCards("實體商品", goods, ".goodsSort2");
+  setGoodsCards("虛擬序號", goods, ".goodsSort3");
+  setGoodsCards("奇貨", goods, ".goodsSort4");
   setPointStoreContent();
-  setupScrolling(".Store_Container", ".btn_next", ".btn_pre", 8);
+  setupScrolling(8);
 });
